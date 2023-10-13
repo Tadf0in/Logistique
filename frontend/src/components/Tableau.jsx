b9e5ce19edfd45d4f132ade34baa6b55f7573ba9
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-10-14 01:01:19 +0200

    Change week arrows

--- frontend/src/components/Tableau.jsx
+++ frontend/src/components/Tableau.jsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import Row from "./Row";
 
 export default function Tableau () {
+    const [today, setToday] = useState(new Date())
+    const [weekDays, setWeekDays] = useState([])
+
     const getDaysOfWeek = (day) => {
         let monday = new Date(day.setDate(day.getDate() - day.getDay() + (day.getDay() === 0 ? -6 : 1)))
         let week = []
@@ -9,11 +13,24 @@ export default function Tableau () {
         }
         return week
     }
+
     const dayLetters = ['L', 'M', 'M', 'J', 'V']
-    const weekDays = getDaysOfWeek(new Date())
+    useEffect(() => {
+        setWeekDays(getDaysOfWeek(today))
+    }, [today])
+    
 
     return <div id="tableau">
         <div className="tab">
+            <div className="theader">
+                <button onClick={() => 
+                    setToday((date) => new Date(date.getFullYear(), date.getMonth(), date.getDate() - 7))
+                }>{'<'}</button>
+                
+                <button onClick={() => 
+                    setToday((date) => new Date(date.getFullYear(), date.getMonth(), date.getDate() + 7))
+                }>{'>'}</button>
+            </div>
             <div className="tbody">
                 {
                     weekDays.map((day, i) => 
